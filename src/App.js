e0d8d79e7b6e23ab2bb79df9de93235ab685077a
Scilla <priscilla.ruiz.tech@gmail.com>
2021-07-12 18:22:26 -0700

    protoype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import NavBar from './components/NavBar';
+import Container from '@material-ui/core/Container'
+import {Typography} from '@material-ui/core';
 import './App.css';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
+
+const mainContainer = {
+  marginTop : '115px',
+  backgroundColor: '#cfe8fc',
+  height: '50vh',
+};
+
 function App() {
+  const classes = styles(); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <NavBar/>
+   <Container maxWidth='sm'>
+     <h1 style={mainContainer}>Space Learning Game</h1>
+   </Container>
     </div>
+
   );
 }
 
-export default App;
+export default App
