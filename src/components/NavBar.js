86184e37c728b7951334feb98dc58ddd70d8909c
Scilla <priscilla.ruiz.tech@gmail.com>
2021-07-12 21:22:49 -0700

    added logo and github link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,59 +1,61 @@
 import React from 'react'
 import CustomBtn from './CustomBtn'
-import logo from '../logo.svg'
+import logo from '../astronaut.jpg'
 import logoMobile from '../logoMobile.svg'
-import {Toolbar, Typography} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles';
+import { Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
 
 const styles = makeStyles({
-    bar:{
+    bar: {
         paddingTop: "1.15rem",
         backgroundColor: "#fff",
-        ['@media (max-width:780px)']: { 
-           flexDirection: "column"
-          }
+        ['@media (max-width:780px)']: {
+            flexDirection: "column"
+        }
     },
     logo: {
-        width: "15%", 
-        ['@media (max-width:780px)']: { 
-           display: "none"
-           }
+        paddingRight: "3rem",
+        width: "15%",
+        ['@media (max-width:780px)']: {
+            display: "none"
+        }
     },
-    logoMobile:{
-        width: "100%", 
-        display: "none", 
-        ['@media (max-width:780px)']: { 
+    logoMobile: {
+        width: "100%",
+        display: "none",
+        ['@media (max-width:780px)']: {
             display: "inline-block"
-            }
+        }
     },
     menuItem: {
-        cursor: "pointer", 
+        cursor: "pointer",
         flexGrow: 1,
         "&:hover": {
-            color:  "#4f25c8"
+            color: "#4f25c8"
         },
-        ['@media (max-width:780px)']: { 
-            paddingBottom: "1rem"    }
+        ['@media (max-width:780px)']: {
+            paddingBottom: "1rem"
+        }
     }
 })
 
 function NavBar() {
     const classes = styles()
     return (
-        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src={logo} className={classes.logo}/> 
-                <img src={logoMobile} className={classes.logoMobile}/> 
-                <Typography variant="h6" className={classes.menuItem}>
-                   About Space Learning Game
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Space Education Crisis
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Priscilla (Ruiz) Seal
-                </Typography>
-                <CustomBtn txt="See Github"/>
-            </Toolbar>
+        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>
+            <img src={logo} className={classes.logo} />
+            <Typography variant="h6" className={classes.menuItem}>
+                About Space Learning Game
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Space Education Crisis
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Priscilla (Ruiz) Seal
+            </Typography>
+            <Button variant="contained" color="primary" href="https://github.com/PRRegis/spaceLearning">Github</Button>
+        </Toolbar>
     )
 }
 
